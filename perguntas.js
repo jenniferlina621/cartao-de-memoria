9d13e98252221f83ed30882442b9a548505d05c4
jenniferlina621 <00001072565122sp@al.educacao.sp.gov.br>
2024-11-21 10:35:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'filosofia',
+    'De quem e a famosa frase "penso, logo existo"?',
+    'Descartes'
+)
+
+criaCartao(
+    'corpo humano',
+    'Quantos dias podemos ficar sem agua?',
+    '3 dias'
+)
+
+criaCartao(
+    'Biologia',
+    'qual e o coletivo de borboleta?',
+    'Panapana'
+)
+
+criaCartao(
+    'Lingua portuguesa',
+    'O morango e classificado como?',
+    'um substantivo'
+)
